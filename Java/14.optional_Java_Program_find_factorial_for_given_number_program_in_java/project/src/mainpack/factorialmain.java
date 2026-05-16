c194fce560d915af0a872e52a2411480d6c6be27
Rawad2003 <rawad_isayed@yahoo.com>
2026-05-16 17:07:26 +0300

    Add 14) (optional) Java program find factorail for given number in java

---
+++ Java/14.optional_Java_Program_find_factorial_for_given_number_program_in_java/project/src/mainpack/factorialmain.java
@@ -0,0 +1,18 @@
+package mainpack;
+
+import java.util.Scanner;
+
+public class factorialmain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number to find the factorial: ");
+		int num = sc.nextInt();
+		int result = 1;
+		for (int i = 1; i <= num; i++)
+			result *= i;
+		System.out.println("the factorial of " + num + " is: " + result);
+	}
+
+}
