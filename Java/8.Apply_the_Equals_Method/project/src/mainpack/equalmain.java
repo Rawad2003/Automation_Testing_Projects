178ffefe99fde7633461d4764ba7ea7e9907bbce
Rawad2003 <rawad_isayed@yahoo.com>
2026-05-16 16:42:52 +0300

    Add 8)Applying the Equals Method

---
+++ Java/8.Apply_the_Equals_Method/project/src/mainpack/equalmain.java
@@ -0,0 +1,15 @@
+package mainpack;
+
+import java.util.Scanner;
+
+public class equalmain {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		Integer num1=sc.nextInt();
+		Integer num2=sc.nextInt();
+		System.out.println(num1.equals(num2));
+	}
+
+}
