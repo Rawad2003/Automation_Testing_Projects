743ce136316ff81e1211257cd68de0f9ffb92ec1
Rawad2003 <rawad_isayed@yahoo.com>
2026-05-16 19:04:03 +0300

    Add 20) add simple code firefox driver

---
+++ Selenium/20.simple_code_firefox_driver/project/src/test/java/TestDriver.java
@@ -0,0 +1,21 @@
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import java.net.URL;
+
+public class TestDriver extends BaseSetupManager {
+	public static void main(String[] args) throws Exception {
+//		// this will be written in the before test as precondition
+//		FirefoxOptions options = new FirefoxOptions();
+//		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//		WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444"), options);
+
+		// this is the test
+//		driver.get("https://www.google.com/");
+		System.out.println("Title: " + driver.getTitle());
+		driver.manage().window().maximize();
+
+		// this will be written in after test method
+//		driver.quit();
+	}
+}
